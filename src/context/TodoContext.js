45b2d77855f6386b48268e211f3c5e60d57906bc
bhojane.v <bhojane.v@internal.media.net>
2022-04-04 14:06:33 +0530

    bug fixes in context

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -105,7 +105,7 @@ export const TodoProvider = ({ children }) => {
         localStorage.setItem('myTodoList', JSON.stringify(items))
         checkHasCompltedTask()
 
-    }, [items])
+    }, [items,checkHasCompltedTask])
 
     return (
         <TodoContext.Provider
